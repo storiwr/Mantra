e65358866062c99af515783a32abef927a5a0aa3
Storiwr <storiwr@users.noreply.github.com>
2020-09-04 15:02:03 -0400

    Initial Commit

---
+++ Mantra/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mantra
+//
+//  Created by Stephen on 9/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mantra/MantraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MantraApp.swift
+//  Mantra
+//
+//  Created by Stephen on 9/4/20.
+//
+
+import SwiftUI
+
+@main
+struct MantraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
